0f03e256e3ebca8d038f9164fb956633539b1ede
targetdubai2026 <targetdubai2026@gmail.com>
2026-03-08 11:17:29 -0700

    Changes

--- src/components/layout/HomeDesignLayout.tsx
+++ src/components/layout/HomeDesignLayout.tsx
@@ -1,14 +1,23 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { HomeHeader } from "./HomeHeader";
 import { HomeFooter } from "./HomeFooter";
 import { MobileNav } from "./MobileNav";
 import { WhatsAppButton } from "./WhatsAppButton";
+
+const BODY_SCROLLBAR_CLASS = "home-design-scrollbar";
 
 interface HomeDesignLayoutProps {
   children: ReactNode;
 }
 
 export function HomeDesignLayout({ children }: HomeDesignLayoutProps) {
+  useEffect(() => {
+    document.body.classList.add(BODY_SCROLLBAR_CLASS);
+    return () => {
+      document.body.classList.remove(BODY_SCROLLBAR_CLASS);
+    };
+  }, []);
+
   return (
     <div className="home-design min-h-screen flex flex-col bg-background pb-16 md:pb-0">
       <HomeHeader />
